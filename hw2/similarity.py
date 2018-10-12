645942ff7a527f0aaa71bca2478fd4aa18ffdc8a
Yu Gu <guyu980@gmail.com>
2018-10-12 03:08:38 -0700

    add similarity.py README

--- hw2/similarity.py
+++ hw2/similarity.py
@@ -4,68 +4,49 @@ import time
 # Define a function to preprocess data, generate list of movie and dictionaries of ratings and users for each movie
 def preprocess_data(data):
 
-    dataDict, userDict = {}, {}
+    dataDict = {}
+    userSet, mvSet = set(), set()
     n = len(data)
-    mvSet = set()
-    userSet = set()
 
-    # Convert rating and movie id into integer, store movie id and user id into set
+    # Generate dataDict[movie id][user id] = rating
     for i in range(n):
-        userSet.add(int(data[i][0]))
-        mvSet.add(int(data[i][1]))
-        data[i][2] = int(data[i][2])
+        user, mv, rating = data[i][0:3]
+        userSet.add(user)
+        mvSet.add(mv)
 
-    # Convert set into list for sorting
-    mvList = list(mvSet)
-    userList = list(userSet)
-    mvList.sort()
-    userList.sort()
+        if mv not in dataDict:
+            dataDict[mv] = {}
 
-    # Convert movie id into string for output
-    # Generate dataDict[movie id][user id] = rating, userDict[movie id] = set of users rating on this movie 
-    for i in range(len(mvList)):
-        mv = str(mvList[i])
-        mvList[i] = mv
-        dataDict[mv] = {}
-        userDict[mv] = set()
+        dataDict[mv][user] = int(rating)
 
-    # Put data into dictionaries
-    for i in range(n):
-        user = data[i][0]
-        mv = data[i][1]
-        userDict[mv].add(data[i][0])
-        dataDict[mv][user] = data[i][2]
+    # Convert movie id and user id into int for sorting, then convert back to string for output
+    userList = list(map(str, sorted(list(map(int, list(userSet))))))
+    mvList = list(map(str, sorted(list(map(int, list(mvSet))))))
 
-    return mvList, userList, dataDict, userDict
+    return mvList, userList, dataDict
 
 # Define a funticon to calculate the similarity of two movies
-def similarity(mv1, mv2, dataDict, userDict, user_thresh):
+def similarity(mv1, mv2, dataDict, user_thresh):
 
     # Extract rating and user data of two movies from dictionaries, get the set of users rating for both movies
-    data1, data2, user1, user2 = dataDict[mv1], dataDict[mv2], userDict[mv1], userDict[mv2]
+    data1, data2 = dataDict[mv1], dataDict[mv2]
+    user1, user2 = set(data1.keys()), set(data2.keys())
     userCommon = user1 & user2
-    # Set a parameter p less than -1 to represent the similarity in some special situations
+    # Set a default value less than -1 to represent the similarity in some special situations
     p = -10
 
     # When common user is not enough or calculating the same two movies, output similarity as p = -10
     if len(userCommon) >= user_thresh and mv1 != mv2:
-        r1, r2, r1_all, r2_all = [], [], [], []
-
-        for user in user1:
-            r1_all.append(data1[user])
-
-        for user in user2:
-            r2_all.append(data2[user])
+        r1, r2 = [], []
+        r1_all, r2_all = list(data1.values()), list(data2.values())
 
         for user in userCommon:
             r1.append(data1[user])
             r2.append(data2[user])
 
         # Calculate average of all ratings for each movie
-        r1_avg = sum(r1_all) / len(r1_all)
-        r2_avg = sum(r2_all) / len(r2_all)
-        r1 = [x - r1_avg for x in r1]
-        r2 = [x - r2_avg for x in r2]
+        r1_avg, r2_avg = sum(r1_all) / len(r1_all), sum(r2_all) / len(r2_all)
+        r1, r2 = [x - r1_avg for x in r1], [x - r2_avg for x in r2]
         s1, s2, s3 = 0, 0, 0
 
         # Calculate similarity
@@ -81,13 +62,12 @@ def similarity(mv1, mv2, dataDict, userDict, user_thresh):
     return p, len(userCommon)
 
 # Define a function to calculate the similarity for all movie pairs
-def similarityAll(mvList, dataDict, userDict, user_thresh):
+def similarityAll(mvList, dataDict, user_thresh):
 
     n = len(mvList)
-    simList = []
-    commonList = []
-
-    # Calculate similarity and number of common user for movie i and movie j (i > j), store them into List[i][j] and List[j][i]
+    simList, commonList = [], []
+ 
+    # Calculate similarity and number of common users for movie i and movie j (i > j), store them into List[i][j] and List[j][i]
     for i in range(n):
         simList.append([])
         commonList.append([])
@@ -97,7 +77,7 @@ def similarityAll(mvList, dataDict, userDict, user_thresh):
             commonList[i].append(commonList[j][i])
 
         for j in range(i, n):
-            p, nCommon = similarity(mvList[i], mvList[j], dataDict, userDict, user_thresh)
+            p, nCommon = similarity(mvList[i], mvList[j], dataDict, user_thresh)
             simList[i].append(p)
             commonList[i].append(nCommon)
 
@@ -115,14 +95,15 @@ def output(mvList, simList, commonList):
         simMax = max(simList[i])
         result = mv
 
-        if simMax > -1:
+        if simMax >= -1:
             mvMax = mvList[simList[i].index(simMax)]
             nCommon = commonList[i][simList[i].index(simMax)]
-            result += ' (' + mvMax + ',' + str('%.4f'%simMax) + ',' +  str(nCommon) + ')'
+            result += ' (' + mvMax + ', ' + str('%.6f'%simMax) + ', ' +  str(nCommon) + ')'
             results.append(result)
 
     return results
 
+# Define the main function of whole program
 def main():
 
     # Print useful message if no arguments given
@@ -145,8 +126,8 @@ def main():
 
     # Calculate similarities and output
     timeStart = time.time()
-    mvList, userList, dataDict, userDict = preprocess_data(data)
-    simList, commonList = similarityAll(mvList, dataDict, userDict, user_thresh)
+    mvList, userList, dataDict = preprocess_data(data)
+    simList, commonList = similarityAll(mvList, dataDict, user_thresh)
     results = output(mvList, simList, commonList)
     timeEnd = time.time()
 
