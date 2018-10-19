b67eed1a6ca961e5af7675b0c8634236c4bb78a9
Yu Gu <guyu980@gmail.com>
2018-10-18 19:14:37 -0700

    add airfoil.py README

--- hw3/airfoil.py
+++ hw3/airfoil.py
@@ -3,7 +3,7 @@ import math
 import os
 
 class Airfoil():
-    """ Define a class Airfoil to deal with the whole problem 
+    """ Define a class Airfoil to deal with the whole problem
 
     Example usage:
         > a = Airfoil(inputdir)
@@ -16,30 +16,32 @@ class Airfoil():
         Args:
             inputdir: Input data directory
         """
-        # Strip '/' first and then add back in case that input directory doesn't have '/'
-        self.inputdir = inputdir.strip('/')+'/'
-        self.xy = None
-        self.cp = {}
         self.cl = {}
         self.stagnation_pt = {}
-        self.load_data()
-        self.compute_chord()
-        for alpha in self.cp:
-            cx, cy = self.cp_decomposition(alpha)
-            self.compute_lift_coef(cx, cy, alpha)
-            self.find_stagnation_point(alpha)
-        self.summary()
+        # Strip '/' first and then add back in case that input directory doesn't have '/'
+        xy, cp = self.load_data(inputdir.strip('/')+'/')
+        self.chord = self.compute_chord(xy)
+        for alpha in cp:
+            cx, cy = self.cp_decomposition(xy, cp[alpha])
+            self.cl[alpha] = self.compute_lift_coef(cx, cy, alpha)
+            self.stagnation_pt[alpha] = self.find_stagnation_point(xy, cp[alpha])
 
 
-    def load_data(self):
+    def load_data(self, inputdir):
         """ Load coordinate and pressure coefficient data from input directory
+
+        Args:
+            inputdir: Input data directory
+        Returns:
+            xy: Coordinate of panel points
+            cp: Dictionary of pressure coefficient of each panel under different degree of attack
         """
         # Get the xy and cp data file name from directory
         # Report error message when directory is invalid
-        if os.path.isdir(self.inputdir) is False:
+        if os.path.isdir(inputdir) is False:
             raise RuntimeError("The directory is invalid")
-        xy_data = glob.glob(self.inputdir+'xy.dat')
-        cp_data = glob.glob(self.inputdir+'alpha*.dat')
+        xy_data = glob.glob(inputdir+'xy.dat')
+        cp_data = glob.glob(inputdir+'alpha*.dat')
 
         # Report error message when no required data file in the directory
         if len(xy_data) == 0:
@@ -53,48 +55,56 @@ class Airfoil():
         # Report error message when having trouble with reading data file
         try:
             with open(xy_data[0]) as f:
-                self.xy = [x.strip().split() for x in f.readlines()]
-                self.xy = [list(map(float, x)) for x in self.xy[1:]]
+                xy = [x.strip().split() for x in f.readlines()]
+                xy = [list(map(float, x)) for x in xy[1:]]
         except:
             raise RuntimeError("Error in reading xy data file")
 
         try:
+            cp = {}
             # Define some keywords from file name to be deleted to get the alpha value
-            keywords = [self.inputdir, 'alpha', '.dat']
+            keywords = [inputdir, 'alpha', '.dat']
             for data in cp_data:
                 with open(data) as f:
                     for kw in keywords:
                         data = data.replace(kw, '')
                     alpha = float(data)
-                    self.cp[alpha] = [x.strip().split() for x in f.readlines()]
-                    self.cp[alpha] = [float(x[0]) for x in self.cp[alpha][1:]]
+                    cp[alpha] = [x.strip().split() for x in f.readlines()]
+                    cp[alpha] = [float(x[0]) for x in cp[alpha][1:]]
         except:
             raise RuntimeError("Error in reading cp data file")
 
+        return xy, cp
 
-    def compute_chord(self):
+
+    def compute_chord(self, xy):
         """ Compute the chord length by xy data
+
+        Args:
+            xy: Coordinate of panel points
+        Returns:
+            chord: Chord length of an airfoil
         """
-        x = [x[0] for x in self.xy]
+        x = [x[0] for x in xy]
         # Get leading and tailing edge point
         leading, trailing = x.index(max(x)), x.index(min(x))
-        self.chord = math.sqrt(sum([(self.xy[leading][i] - self.xy[trailing][i]) ** 2 for i in range(2)]))
+        return math.sqrt(sum([(xy[leading][i] - xy[trailing][i]) ** 2 for i in range(2)]))
 
 
-    def cp_decomposition(self, alpha):
+    def cp_decomposition(self, xy, cp):
         """ Do pressure coefficient decomposition for each panel, calculate total Cartesian force coefficients
 
         Args:
-            alpha: Angle of attack
+            xy: Coordinate of panel points
+            cp: Pressure coefficient of each panel under specific degree of attack
         Returns:
             sum(cx): Total Cartesian force coefficients on direction x
             sum(cy): Total Cartesian force coefficients on direction y
         """
-        cp = self.cp[alpha]
         cx, cy = [], []
         n = len(cp)
         for i in range(n):
-            pt1, pt2 = self.xy[i], self.xy[i+1]
+            pt1, pt2 = xy[i], xy[i+1]
             delta_x, delta_y = pt2[0] - pt1[0], pt2[1] - pt1[1]
             cx.append(-cp[i] * delta_y / self.chord)
             cy.append(cp[i] * delta_x / self.chord)
@@ -109,33 +119,43 @@ class Airfoil():
             cx: Total Cartesian force coefficients on direction x
             cy: Total Cartesian force coefficients on direction y
             alpha: Angle of attack
+        Returns
+            cl: Life coefficient of an airfoil under specific degree of attack
         """
-        self.cl[alpha] = cy * math.cos(math.radians(alpha)) - cx * math.sin(math.radians(alpha))
+        return cy * math.cos(math.radians(alpha)) - cx * math.sin(math.radians(alpha))
 
 
-    def find_stagnation_point(self, alpha):
+    def find_stagnation_point(self, xy, cp):
         """ Find the stagnation point
 
         Args:
-            alpha: Angle of attack
+            xy: Coordinate of panel points
+            cp: Pressure coefficient of each panel under specific degree of attack
+        Returns:
+            List contained coordinate and pressure coefficient of a stagnation point
         """
-        cp = self.cp[alpha]
         # Find the point with its cp value closest to 1.0
         distance_to_1 = [abs(x - 1) for x in cp]
         index = distance_to_1.index(min(distance_to_1))
-        pt1, pt2 = self.xy[index], self.xy[index+1]
+        pt1, pt2 = xy[index], xy[index+1]
         x, y = (pt2[0] + pt1[0])/2, (pt2[1] + pt1[1])/2
-        self.stagnation_pt[alpha] = [x, y, cp[index]]
+
+        return [x, y, cp[index]]
 
 
-    def summary(self):
+    def __repr__(self):
         """ Print results
+
+        Returns:
+            summary: String of summary of results
         """
-        print("alpha     cl           stagnation pt      ")
-        print("-----  -------  --------------------------")
-        alphaList = list(self.cp.keys())
+        summary = "alpha     cl           stagnation pt      \n" + \
+                  "-----  -------  --------------------------\n"
+        alphaList = list(self.cl.keys())
         alphaList.sort()
         for alpha in alphaList:
             pt = self.stagnation_pt[alpha]
-            print("{:5.2f}  {:7.4f}  ({:7.4f}, {:7.4f}) {:7.4f}".format(alpha, self.cl[alpha], pt[0], pt[1], pt[2]))
+            summary += "{:5.2f}  {:7.4f}  ({:7.4f}, {:7.4f}) {:7.4f}\n".format(alpha, self.cl[alpha], pt[0], pt[1], pt[2])
+
+        return summary
 
