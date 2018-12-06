518b63834f53524b853813953cc6b0d9f8d04ab7
Yu Gu <32755499+guyu980@users.noreply.github.com>
2018-12-06 12:27:59 -0800

    Create CashRegister.hpp

---
+++ exam2/CashRegister.hpp
@@ -0,0 +1,11 @@
+#ifndef CashRegister_hpp
+#define CashRegister_hpp
+
+class CashRegister {
+    float change;
+  public:
+    CashRegister(float change);
+    void Print(void);
+};
+
+#endif /* CashRegister_hpp */
